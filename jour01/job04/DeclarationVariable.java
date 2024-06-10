1394a00d002169fba22663f8724cf5fb82699db2
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-10 09:41:44 +0200

    job 4 done

---
+++ jour01/job04/DeclarationVariable.java
@@ -0,0 +1,11 @@
+package jour01.job04;
+
+public class DeclarationVariable {
+    public static void main(String[] args) {
+        int number1, number2, number3;
+        number1 = number2 = number3 = 0;
+        System.out.println("number1 = " + number1);
+        System.out.println("number2 = " + number2);
+        System.out.println("number3 = " + number3);
+    }
+}
