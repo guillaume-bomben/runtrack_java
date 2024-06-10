025248533311f40bd191fd70f3dc9368e6233c4f
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-10 09:51:36 +0200

    job 7 done

---
+++ jour01/job07/Addition.java
@@ -0,0 +1,10 @@
+package jour01.job07;
+
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 3, num2 = 73;
+        double result = (double) num1 / num2;
+        
+        System.out.println("La division de " + num1 + " par " + num2 + " est " + result);
+    }
+}
