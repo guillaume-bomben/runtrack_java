5fd30a36f194bcf65a3f92969c7fc02e1d005bd7
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-10 10:02:47 +0200

    job 11 done

---
+++ jour01/job11/Main.java
@@ -0,0 +1,17 @@
+package jour01.job11;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez un temps en minute : ");
+        int minutes = scanner.nextInt();
+        int heures = minutes / 60;
+        int minutesRestantes = minutes % 60;
+
+        System.out.println(minutes + " minutes équivalent à " + heures + " heures et " + minutesRestantes + " minutes.");
+
+        scanner.close();
+    }
+}
