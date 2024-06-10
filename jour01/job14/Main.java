8a7ebe0e98204d1bedee2df47e5bc9d2e5c36c02
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-10 10:10:46 +0200

    job 14 done

---
+++ jour01/job14/Main.java
@@ -0,0 +1,9 @@
+package jour01.job14;
+
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 5, number2 = 10;
+        Somme somme = new Somme(number1, number2);
+        System.out.println("La somme de " + number1 + " et " + number2 + " est " + somme.calculerSomme());
+    }
+}
