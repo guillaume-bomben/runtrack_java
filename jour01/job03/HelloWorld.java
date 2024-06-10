35481282a1f231d0eaf472a1b22eb70934cb81a6
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-10 09:32:00 +0200

    job 3 done

---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    //Ceci est un commentaire sur une seule ligne
+
+    /*
+    Ceci est un commentaire sur plusieurs lignes
+    */
+}
