f1f064698a4acefe2aae3b804feb09d6e8a4dfcc
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-10 09:47:05 +0200

    job 6 done

---
+++ jour01/job06/ExerciceVariables.java
@@ -0,0 +1,13 @@
+package jour01.job06;
+
+public class ExerciceVariables {
+    public static void main(String[] args) {
+        int num1 = 1 , num2 = 2;
+        System.out.println("La valeur de num1 est " + num1 + " et la valeur de num2 est " + num2);
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("La valeur de num1 est " + num1 + " et la valeur de num2 est " + num2);
+    }
+}
