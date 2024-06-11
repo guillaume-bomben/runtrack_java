fd0b4894ee75d638868dde16dcbb107eec3cdd45
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-11 09:43:10 +0200

    job 9 done

---
+++ jour02/job09/Main.java
@@ -0,0 +1,9 @@
+package jour02.job09;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 1234;
+        System.out.println("Le premier chiffre de " + number + " est : " + String.valueOf(number).charAt(0));
+        System.out.println("Le dernier chiffre de " + number + " est : " + String.valueOf(number).charAt(3));
+    }
+}
