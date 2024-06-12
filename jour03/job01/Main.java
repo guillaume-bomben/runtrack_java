014154b764b0aebbcae4582922b97886fa780565
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-12 09:24:14 +0200

    job 1 done

---
+++ jour03/job01/Main.java
@@ -0,0 +1,12 @@
+package jour03.job01;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] tableau = {10, 0, 2, 0, 69};
+
+        for (int i : tableau) {
+            System.out.println(i);
+        }
+        System.out.println("index 1  : " + tableau[1]);
+    }
+}
