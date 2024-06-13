a1db42f4ce463f957b57c6d19459aa7b312df73c
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-06-13 09:43:12 +0200

    job 1 done

---
+++ jour04/job01/MyThread/MyThread.java
@@ -0,0 +1,8 @@
+package jour04.job01.MyThread;
+
+public class MyThread extends Thread {
+
+    public void run() {
+        System.out.println("Mon thread est en cours");
+    }
+}
---
+++ jour04/job01/ThreadProject/ThreadProject.java
@@ -0,0 +1,10 @@
+package jour04.job01.ThreadProject;
+
+import jour04.job01.MyThread.MyThread;
+
+public class ThreadProject {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
